86b97f54fcdef903881f22ea318d4d30188d4b30
Oleg <lezik569@gmail.com>
2022-12-11 20:15:24 +0200

    2 files added

---
+++ classa.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "classp.h"
+using namespace std;
+
+int main(){
+    SportShoesStore sneakersO;
+    sneakersO.SortPrice();
+    for(int y = 0; y < 5; y++){
+        cout << sneakersO.sneakers[y].brand << "\t";
+        cout << sneakersO.sneakers[y].size << "\t";
+        cout << sneakersO.sneakers[y].color << "\t";
+        cout << sneakersO.sneakers[y].price << "\t";
+        cout << sneakersO.sneakers[y].quantity << "\t";
+        cout << sneakersO.sneakers[y].material << "\t";
+        cout << sneakersO.sneakers[y].number << endl;
+    }
+    cout << endl;
+    sneakersO.SortQuantity();
+    for(int y = 0; y < 5; y++){
+        cout << sneakersO.sneakers[y].brand << "\t";
+        cout << sneakersO.sneakers[y].size << "\t";
+        cout << sneakersO.sneakers[y].color << "\t";
+        cout << sneakersO.sneakers[y].price << "\t";
+        cout << sneakersO.sneakers[y].quantity << "\t";
+        cout << sneakersO.sneakers[y].material << "\t";
+        cout << sneakersO.sneakers[y].number << endl;
+    }
+    cout << endl;
+    sneakersO.SortNumer();
+    for(int y = 0; y < 5; y++){
+        cout << sneakersO.sneakers[y].brand << "\t";
+        cout << sneakersO.sneakers[y].size << "\t";
+        cout << sneakersO.sneakers[y].color << "\t";
+        cout << sneakersO.sneakers[y].price << "\t";
+        cout << sneakersO.sneakers[y].quantity << "\t";
+        cout << sneakersO.sneakers[y].material << "\t";
+        cout << sneakersO.sneakers[y].number << endl;
+    }
+}
